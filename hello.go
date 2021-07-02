cb0aecb9fe79a000b4de60853cfe373a98c52ec5
Michal Materowski <michal.materowski@gmail.com>
2021-07-02 08:01:21 +0200

    appendEntriesRpc

--- hello.go
+++ hello.go
@@ -15,9 +15,24 @@ func main() {
 	lastLogIndex := 0
 	lastLogTerm := 2
 	term, voteGranted := requestVoteRPC(currentTerm, id, lastLogIndex, lastLogTerm)
+	previousLogIndex := 3
+	previousLogTerm := 8
+	entries := []string{"a", "b"}
+	leaderCommitIndex := 7
+
+	appendTerm, success := appendEntriesRPC(currentTerm, id, previousLogIndex, previousLogTerm, entries, leaderCommitIndex)
+	print(appendTerm, success)
 	print(term, voteGranted)
 }
 
 func requestVoteRPC(term int, candidateId ksuid.KSUID, lastLogIndex int, lastLogTerm int) (int, bool) {
-	return 3, true
+	currentTerm := 2
+	voteGranted := true
+	return currentTerm, voteGranted
+}
+
+func appendEntriesRPC(term int, leaderId ksuid.KSUID, previousLogIndex int, previousLogTerm int, entries []string, leaderCommitIndex int) (int, bool) {
+	currentTerm := 5
+	success := true
+	return currentTerm, success
 }
