899c85f6114fccab3b0ed7973a52d24ba9f0d2b5
Michal Materowski <michal.materowski@gmail.com>
2021-07-04 14:06:19 +0200

    persist log as sqlite

--- server/go.mod
+++ server/go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gomodule/redigo v1.8.5 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	github.com/mediocregopher/radix/v3 v3.7.0 // indirect
 	github.com/mediocregopher/radix/v4 v4.0.0-beta.1 // indirect
 	github.com/segmentio/ksuid v1.0.3 // indirect
--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
+	"strconv"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/segmentio/ksuid"
 )
 
@@ -24,6 +27,20 @@ const (
 
 func startServer(id string) {
 	log.Print("Starting server...")
+	database, _ := sql.Open("sqlite3", "../data/log.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement.Exec("Nic", "Raboy")
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	var _id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&_id, &firstname, &lastname)
+		log.Println(strconv.Itoa(_id) + ": " + firstname + " " + lastname)
+	}
+
 	setServerIdFromEnv()
 	connected := ConnectToRedis()
 	if connected {
